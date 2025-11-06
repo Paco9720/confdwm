c111366f577fedb4a73cfc6a8bc94d1ee5edb168
paco9720 <62359183+Paco9720@users.noreply.github.com>
2025-11-06 17:58:17 -0600

    Add files via upload

---
+++ config.h
@@ -0,0 +1,93 @@
+static const unsigned int borderpx  = 0;
+static const unsigned int snap      = 32;
+static const int showbar            = 1;
+static const int topbar             = 1;
+static const int focusonwheel       = 0;
+static const int lockfullscreen     = 1;
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+
+static const char *colors[][3]      = {
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+/* rules */
+static const Rule rules[] = {
+	{ NULL, NULL, NULL, 0, 0, -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55;
+static const int nmaster     = 1;
+static const int resizehints = 1;
+
+static const Layout layouts[] = {
+	{ "[]=", tile },
+	{ "><>", NULL },
+	{ "[M]", monocle },
+};
+
+/* commands */
+static char dmenumon[2] = "0";
+static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *roficmd[]  = { "rofi", "-show", "drun", NULL };
+
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+static void safeKillClient(const Arg *arg);
+
+static Key keys[] = {
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_space,      spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_q,      safeKillClient, {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+};
+
+static Button buttons[] = {
+	{ ClkLtSymbol, 0, Button1, setlayout, {0} },
+	{ ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]} },
+	{ ClkWinTitle, 0, Button2, zoom, {0} },
+	{ ClkStatusText, 0, Button2, spawn, {.v = termcmd } },
+	{ ClkClientWin, MODKEY, Button1, movemouse, {0} },
+	{ ClkClientWin, MODKEY, Button2, togglefloating, {0} },
+	{ ClkClientWin, MODKEY, Button3, resizemouse, {0} },
+	{ ClkTagBar, 0, Button1, view, {0} },
+	{ ClkTagBar, 0, Button3, toggleview, {0} },
+	{ ClkTagBar, MODKEY, Button1, tag, {0} },
+	{ ClkTagBar, MODKEY, Button3, toggletag, {0} },
+};
+
+/* safeKillClient */
+void
+safeKillClient(const Arg *arg) {
+	if (selmon->sel)
+		killclient(arg);
+}
+
